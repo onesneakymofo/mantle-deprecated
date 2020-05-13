b37de07c56231300dd1a397ed022ddc399aa17f5
Chris Autwell <cautwell@gmail.com>
2020-05-13 14:05:55 -0500

    Add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import '../src/assets/tailwind.css';
+
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,47 +1,56 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from 'classnames';
 
 class Button extends React.Component {
-  determinVariant() {
-    switch(this.props.variant) {
-      case 'blue':
-        return ' bg-blue-700 text-white hover:bg-blue-800 '
-      case 'gray':
-        return ' bg-gray-300 text-gray-800 hover:bg-gray-400 '
-      case 'red':
-        return ' bg-red-700 text-white hover:bg-red-800 '
-      case 'white':
-        return ' bg-white border-gray-300 border-2 text-gray-800 '
-      default:
-        return ' bg-blue-700 text-white hover:bg-blue-800 '
-    }
+  buttonColor = () => {
+    const color = this.props.color;
+    return classNames({
+      "bg-blue-700 text-white hover:bg-blue-800": !color || color === "blue",
+      "bg-gray-300 text-gray-800 hover:bg-gray-400": color === "gray",
+      "bg-red-700 text-white hover:bg-red-800": color === "red",
+      "bg-white border-gray-300 border-2 text-gray-800 hover:bg-gray-100": color === "white",
+      "inline-block px-3 py-1 rounded tracking-wider font-semibold cursor-pointer \
+       focus:outline-none focus:shadow-outline": true
+    });
+  }
+  buttonSize = () => {
+    const size = this.props.size;
+    console.log(size);
+    return classNames({
+      "text-xs": size === "xs",
+      "text-sm": size === "sm",
+      "text-lg": size === "lg",
+      "text-xl": size === "xl"
+    });
   }
 
   render() {
     const { text, ...rest } = this.props;
+    console.log(this.buttonColor() + this.buttonSize())
     return (
       <button
-        className = { "inline-block px-3 py-1 rounded tracking-wider font-semibold cursor-pointer focus:outline-none focus:shadow-outline " + this.determinVariant() + this.props.className}
-      >
-      { this.props.text }
+        className = { this.buttonColor() + " " + this.buttonSize() + " " + this.props.className } >
+        { this.props.children }
       </button>
     );
   }
 }
 
 Button.propTypes = {
-  className: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.string,
   disabled: PropTypes.bool,
   type: PropTypes.string,
   hidden: PropTypes.bool,
   role: PropTypes.string,
-  onclick: PropTypes.func
+  onclick: PropTypes.func,
 };
 
 Button.defaultProps = {
+  className: "",
   disabled: false,
-  text: "Submit",
   type: "button",
   hidden: false,
   role: "button"
